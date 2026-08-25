2b167b6bbfcbe72c9ec63d2551e702631d1c88a3
jwchonlada2551-lab <jw.chonlada2551@gmail.com>
2026-08-25 12:45:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใล้ (Optional)
 document.addEventListener('DOMContentLoaded',function () {
+   document.getElementById('year').innerHTML = new Date().getFullYear();
    console.log('Portfolio ของ CHONLADA พร้อมแล้ว!');
 }};
